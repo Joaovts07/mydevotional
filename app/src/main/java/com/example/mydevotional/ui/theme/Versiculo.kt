db5ad5b225c2ae7aa2d92c09bb5855703c604789
joaovts <joakim.vts@gmail.com>
2025-02-28 14:13:01 -0300

    adjust data class Versiculo for random verses

--- app/src/main/java/com/example/mydevotional/ui/theme/Versiculo.kt
+++ app/src/main/java/com/example/mydevotional/ui/theme/Versiculo.kt
@@ -1,10 +1,22 @@
 package com.example.mydevotional.ui.theme
 
 data class Versiculo(
-    val reference: String = "",
-    val text: String = "",
-    val translation_id: String = "",
-    val translation_name: String = "",
-    val translation_language: String = "",
-    val version: String = ""
+    val translation: Translation,
+    val random_verse: RandomVerse
+)
+
+data class Translation(
+    val identifier: String,
+    val name: String,
+    val language: String,
+    val language_code: String,
+    val license: String
+)
+
+data class RandomVerse(
+    val book_id: String,
+    val book: String,
+    val chapter: Int,
+    val verse: Int,
+    val text: String
 )
