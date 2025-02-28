b5f254e25853200b53f212099393f112ee96ef7d
joaovts <joakim.vts@gmail.com>
2025-02-28 19:00:31 -0300

    add libs for navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.material.icons.extended)
 
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
