03d854d735df143364115131ae588eab01de3cc7
joaovts <joakim.vts@gmail.com>
2025-02-27 22:38:47 -0300

    Add homescreen client and mainactivity

--- app/src/main/java/com/example/mydevotional/MainActivity.kt
+++ app/src/main/java/com/example/mydevotional/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mydevotional
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,20 +14,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mydevotional.ui.theme.MyDevotionalTheme
+import com.example.mydevotional.ui.theme.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyDevotionalTheme {
+            /*MyDevotionalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
-            }
+            }*/
+            HomeScreen()
         }
     }
 }
