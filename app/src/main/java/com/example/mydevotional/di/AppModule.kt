44de0956500f23fa4413fda7ff242a6da9a73bc1
joaovts <joakim.vts@gmail.com>
2025-03-04 20:04:46 -0300

    MYD-12:Refactor needs change for object for use Impl

--- app/src/main/java/com/example/mydevotional/di/AppModule.kt
+++ app/src/main/java/com/example/mydevotional/di/AppModule.kt
@@ -2,17 +2,41 @@ package com.example.mydevotional.di
 
 import com.example.mydevotional.repositorie.BibleRepository
 import com.example.mydevotional.repositorie.BibleRepositoryImpl
-import dagger.Binds
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.gson.gson
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AppModule {
+object AppModule {
 
-    @Binds
+    @Provides
     @Singleton
-    abstract fun bindBibleRepository(impl: BibleRepositoryImpl): BibleRepository
+    fun provideBibleRepository(
+        firestore: FirebaseFirestore,
+        httpClient: HttpClient
+    ): BibleRepository {
+        return BibleRepositoryImpl(firestore, httpClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(Android) {
+            install(ContentNegotiation) {
+                gson()
+            }
+        }
+    }
 }
