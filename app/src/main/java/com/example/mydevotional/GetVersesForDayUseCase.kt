6512b4fc46bab4c02a2e40dddad9a744a73e003d
joaovts <joakim.vts@gmail.com>
2025-03-04 19:41:23 -0300

    MYD-1: Add use cases

---
+++ app/src/main/java/com/example/mydevotional/GetVersesForDayUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.mydevotional
+
+import com.example.mydevotional.repositorie.BibleRepository
+import com.example.mydevotional.ui.theme.Verse
+import java.util.Date
+import javax.inject.Inject
+
+class GetVersesForDayUseCase @Inject constructor(
+    private val repository: BibleRepository
+) {
+    suspend operator fun invoke(date: Date): List<Verse> {
+        return repository.getVersesForDay(date)
+    }
+}
