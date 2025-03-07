3699382e3397f6eebef35a9c183bfd557d6b7309
joaovts <joakim.vts@gmail.com>
2025-03-07 18:52:29 -0300

    MYD-5: Refactor add + 1 in list for not show 0 and null

--- app/src/main/java/com/example/mydevotional/ui/theme/screens/VersesScreen.kt
+++ app/src/main/java/com/example/mydevotional/ui/theme/screens/VersesScreen.kt
@@ -1,16 +1,12 @@
 package com.example.mydevotional.ui.theme.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -28,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mydevotional.BibleBook
-import com.example.mydevotional.ui.theme.Verse
 import com.example.mydevotional.viewmodel.VersesViewModel
 
 @Composable
@@ -94,7 +89,8 @@ fun BookListView(books: List<BibleBook>, onBookSelected: (BibleBook) -> Unit) {
 @Composable
 fun ChapterListView(chapters: Int, onChapterSelected: (Int) -> Unit) {
     LazyColumn {
-        items(chapters) { chapter ->
+        items(chapters) { index ->
+            val chapter = index + 1
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
