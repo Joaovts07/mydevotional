3e1446aa94f16b8500630a3d034089f6ef0bf6ba
joaovts <joakim.vts@gmail.com>
2025-03-11 12:25:04 -0300

    MYD-6: Refactor created a Book Screen

---
+++ app/src/main/java/com/example/mydevotional/ui/screens/BooksScreen.kt
@@ -0,0 +1,49 @@
+package com.example.mydevotional.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.mydevotional.viewmodel.VersesViewModel
+
+@Composable
+fun BooksScreen(navController: NavController, viewModel: VersesViewModel = hiltViewModel()) {
+    val books by viewModel.books.collectAsState()
+
+    Column(Modifier.fillMaxSize().padding(16.dp)) {
+        LazyColumn {
+            items(books) { book ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .clickable {
+                            viewModel.selectBook(book)
+                            navController.navigate("chapters/${book.name}")
+                        },
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+                    Text(
+                        text = book.name,
+                        modifier = Modifier.padding(16.dp),
+                        fontSize = 18.sp
+                    )
+                }
+            }
+        }
+    }
+}
