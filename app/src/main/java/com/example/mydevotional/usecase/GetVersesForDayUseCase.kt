8ce75c87c13493fad542f9853ffdb820796e1889
joaovts <joakim.vts@gmail.com>
2025-03-19 08:10:22 -0300

    MYD-7: Refactor populate field textChapter

--- app/src/main/java/com/example/mydevotional/usecase/GetVersesForDayUseCase.kt
+++ app/src/main/java/com/example/mydevotional/usecase/GetVersesForDayUseCase.kt
@@ -10,17 +10,17 @@ class GetVersesForDayUseCase @Inject constructor(
     private val repository: BibleRepository
 ) {
     suspend operator fun invoke(date: Date): List<Verses> {
-        return getVersesForDay(repository.getVersesForDay(date))
+        val bibleResponses = repository.getVersesForDay(date)
+        return transformBibleResponsesToVerses(bibleResponses)
     }
 
-    private fun getVersesForDay(verses: List<BibleResponse>): List<Verses> {
-        val versesForday = mutableListOf<Verses>()
-        verses.forEach {
-            it.verses.forEach { verse ->
-                versesForday.add(verse)
+    private fun transformBibleResponsesToVerses(bibleResponses: List<BibleResponse>): List<Verses> {
+        val versesForDay = mutableListOf<Verses>()
+        bibleResponses.forEach { bibleResponse ->
+            bibleResponse.verses.forEach { verse ->
+                versesForDay.add(verse.copy(textChapter = bibleResponse.text))
             }
-
         }
-        return versesForday
+        return versesForDay
     }
 }
