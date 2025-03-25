6f5e0ff0d80757ce81920cd2c0e0fae5e538b092
joaovts <joakim.vts@gmail.com>
2025-03-25 19:39:50 -0300

    MYD-7: adjust for use only one LazyColumn in screen

--- app/src/main/java/com/example/mydevotional/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mydevotional/ui/screens/HomeScreen.kt
@@ -1,14 +1,30 @@
 package com.example.mydevotional.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.MenuBook
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -19,14 +35,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.mydevotional.ReadVerseWithTTS
 import com.example.mydevotional.components.CalendarReadings
-import com.example.mydevotional.components.DisplayModeContent
+import com.example.mydevotional.model.BibleResponse
+import com.example.mydevotional.model.Verses
 import com.example.mydevotional.viewmodel.HomeScreenViewModel
 
 @Composable
@@ -39,28 +56,17 @@ fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
     var isSingleCardMode by remember { mutableStateOf(true) }
     val listState = rememberLazyListState()
 
-    // Controle de scroll para efeito de Parallax
-    val nestedScrollConnection = remember {
-        object : NestedScrollConnection {
-            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                val minHeight = 80.dp
-                val maxHeight = 250.dp
-                val newHeight = (calendarHeight - available.y.dp).coerceIn(minHeight, maxHeight)
-                calendarHeight = newHeight
-                return Offset.Zero
-            }
-        }
+    LaunchedEffect(remember { derivedStateOf { listState.firstVisibleItemScrollOffset } }) {
+        val minHeight = 80.dp
+        val maxHeight = 250.dp
+        calendarHeight = (maxHeight - (listState.firstVisibleItemScrollOffset / 5).dp).coerceIn(minHeight, maxHeight)
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(nestedScrollConnection)
+    LazyColumn(
+        state = listState,
+        modifier = Modifier.fillMaxSize()
     ) {
-
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
+        item {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -73,7 +79,10 @@ fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
                     onDateSelected = { selectedDate -> viewModel.selectDate(selectedDate) }
                 )
             }
-            if (isLoading) {
+        }
+
+        if (isLoading) {
+            item {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -82,17 +91,131 @@ fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
                 ) {
                     CircularProgressIndicator()
                 }
+            }
+        } else {
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(
+                        onClick = { isSingleCardMode = true },
+                        modifier = Modifier.size(40.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Book,
+                            contentDescription = "Show full chapter",
+                            tint = if (isSingleCardMode) MaterialTheme.colorScheme.primary else Color.Gray
+                        )
+                    }
 
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    IconButton(
+                        onClick = { isSingleCardMode = false },
+                        modifier = Modifier.size(40.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.MenuBook,
+                            contentDescription = "Show verse by verse",
+                            tint = if (!isSingleCardMode) MaterialTheme.colorScheme.primary else Color.Gray
+                        )
+                    }
+                }
+            }
+
+            if (isSingleCardMode) {
+                items(bibleResponse) { bibleResponse ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Text(
+                                text = bibleResponse.text,
+                                fontSize = 18.sp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+
+                            Text(
+                                text = "- ${bibleResponse.reference} ",
+                                fontStyle = FontStyle.Italic,
+                                modifier = Modifier.padding(top = 4.dp)
+                            )
+                        }
+                    }
+                }
             } else {
-                DisplayModeContent(
-                    isSingleCardMode = isSingleCardMode,
-                    onModeChange = { isSingleCardMode = it },
-                    bibleResponses = bibleResponse
-                )
+                items(bibleResponse.flatMap { it.verses }) { verse ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Text(
+                                text = verse.text,
+                                fontSize = 18.sp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "- ${verse.book_name} ${verse.chapter}:${verse.verse}",
+                                    fontStyle = FontStyle.Italic
+                                )
+
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    IconButton(
+                                        onClick = {
+                                            //onFavoriteClick(verse)
+                                        }
+                                    ) {
+                                        Icon(
+                                            imageVector = if (verse.isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                                            contentDescription = "Favoritar verso",
+                                            tint = if (verse.isFavorite) Color.Red else Color.Gray
+                                        )
+                                    }
+                                    ReadVerseWithTTS(verse.text)
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-}  
+}
+
+@Composable
+fun CombinedDisplayContent(
+    bibleResponses: List<BibleResponse> = emptyList(),
+    onFavoriteClick: (Verses) -> Unit = {}
+) {
+    var isSingleCardMode by remember { mutableStateOf(true) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
 
 
 
