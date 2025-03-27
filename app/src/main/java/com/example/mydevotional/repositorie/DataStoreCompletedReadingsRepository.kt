356cf09765ac2d203d5c0d3de34591e95d589232
joaovts <joakim.vts@gmail.com>
2025-03-27 11:43:17 -0300

    MYD-12: Add Repositories

---
+++ app/src/main/java/com/example/mydevotional/repositorie/DataStoreCompletedReadingsRepository.kt
@@ -0,0 +1,27 @@
+package com.example.mydevotional.repositorie
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class DataStoreCompletedReadingsRepository @Inject constructor(private val context: Context) :
+    CompletedReadingsRepository {
+
+    private val COMPLETED_READINGS_KEY = stringSetPreferencesKey("completed_readings")
+
+    override suspend fun markReadingAsComplete(date: String) {
+        context.dataStore.edit { preferences ->
+            val currentReadings = preferences[COMPLETED_READINGS_KEY] ?: emptySet()
+            preferences[COMPLETED_READINGS_KEY] = currentReadings + date
+        }
+    }
+
+    override fun getCompletedReadings(): Flow<Set<String>> {
+        return context.dataStore.data.map { preferences ->
+            preferences[COMPLETED_READINGS_KEY] ?: emptySet()
+        }
+    }
+}
