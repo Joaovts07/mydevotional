fa8338b7eecdbf61aa0ea58356360ca0e7b52169
joaovts <joakim.vts@gmail.com>
2025-04-05 21:41:57 -0300

    MYD-14: Refactor add a verification for not click a one date

--- app/src/main/java/com/example/mydevotional/usecase/MarkReadingAsCompleteUseCase.kt
+++ app/src/main/java/com/example/mydevotional/usecase/MarkReadingAsCompleteUseCase.kt
@@ -1,12 +1,18 @@
 package com.example.mydevotional.usecase
 
+import com.example.mydevotional.extensions.formatDate
 import com.example.mydevotional.repositorie.CompletedReadingsRepository
+import java.util.Date
 import javax.inject.Inject
 
 class MarkReadingAsCompleteUseCase @Inject constructor(
     private val completedReadingsRepository: CompletedReadingsRepository
 ){
     suspend operator fun invoke(readingId: String) {
-        completedReadingsRepository.markReadingAsComplete(readingId)
+        if (readingId.isEmpty()) {
+            completedReadingsRepository.markReadingAsComplete(Date().formatDate("yyyy-MM-dd"))
+        } else {
+            completedReadingsRepository.markReadingAsComplete(readingId)
+        }
     }
 }
