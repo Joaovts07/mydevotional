a1cbd0cf79fb4df09be89275774fc054f80799e1
João Vitor Targino <joakim.vts@gmail.com>
2025-08-10 20:07:53 -0300

    MYD-13: Adjust detekt imports

--- login/src/main/java/com/example/login/ui/screens/LoginScreen.kt
+++ login/src/main/java/com/example/login/ui/screens/LoginScreen.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
