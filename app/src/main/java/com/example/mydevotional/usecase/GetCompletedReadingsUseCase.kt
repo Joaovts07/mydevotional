553fa02e499a6c2e418b1e4c0aee2da0300fa86e
João Vitor Targino <joakim.vts@gmail.com>
2025-09-03 11:16:52 -0300

    MYD-30:Change the GetCompletedReadingsUseCase

--- app/src/main/java/com/example/mydevotional/usecase/GetCompletedReadingsUseCase.kt
+++ app/src/main/java/com/example/mydevotional/usecase/GetCompletedReadingsUseCase.kt
@@ -3,11 +3,19 @@ package com.example.mydevotional.usecase
 import com.example.mydevotional.repositorie.CompletedReadingsRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class GetCompletedReadingsUseCase @Inject constructor(
-    private val completedReadingsRepository: CompletedReadingsRepository
-){
-    operator fun invoke(): Flow<List<String>> {
-        return completedReadingsRepository.getCompletedReadings()
+@Singleton
+class CompleteReadingsUseCase @Inject constructor(
+    private val repository: CompletedReadingsRepository
+) {
+    fun getCompletedReadingsFlow(): Flow<Set<String>> {
+        return repository.getCompletedReadings()
     }
+
+    suspend fun toggleCompletion(date: String) {
+        repository.toggleReadingComplete(date)
+    }
+
 }
+
