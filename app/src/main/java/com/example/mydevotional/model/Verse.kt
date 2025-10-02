200c956e8238e8b90c8ff8c1677542ee518697a9
João Vitor Targino <joakim.vts@gmail.com>
2025-10-02 08:15:41 -0300

    MYD-36: Fix add a serial name for show the name of the book

--- app/src/main/java/com/example/mydevotional/model/Verse.kt
+++ app/src/main/java/com/example/mydevotional/model/Verse.kt
@@ -1,7 +1,10 @@
 package com.example.mydevotional.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Verses(
     val bookId: String = "",
+    @SerializedName("book_name")
     val bookName: String = "",
     val chapter: Int = 0,
     val verse: Int = 0,
