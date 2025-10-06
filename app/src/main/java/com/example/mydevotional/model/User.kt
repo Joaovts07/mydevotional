761764c29b67154c208212e5e63d5008f4db1653
João Vitor Targino <joakim.vts@gmail.com>
2025-10-06 11:47:18 -0300

    MYD-38: Add a new model for Users

---
+++ app/src/main/java/com/example/mydevotional/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.mydevotional.model
+
+data class User(
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val translation: String = "NVI" 
+)
