86a80b047e8fdb84509011d0ed81a9a9260bc8a8
João Vitor Targino <joakim.vts@gmail.com>
2025-10-08 22:48:16 -0300

    MYD-38: Add a UserRemoteDataSource

---
+++ app/src/main/java/com/example/mydevotional/remote/UserRemoteDataSource.kt
@@ -0,0 +1,31 @@
+package com.example.mydevotional.remote
+
+import com.example.mydevotional.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class UserRemoteDataSource(
+    private val auth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
+) {
+    suspend fun fetchUser(): User? {
+        val userId = auth.currentUser?.uid ?: return null
+        val snapshot = firestore.collection("users")
+            .document(userId)
+            .get()
+            .await()
+
+        return snapshot.toObject(User::class.java)?.copy(id = userId)
+    }
+
+    fun listenUser(onUserChanged: (User?) -> Unit) {
+        val userId = auth.currentUser?.uid ?: return
+        firestore.collection("users")
+            .document(userId)
+            .addSnapshotListener { snapshot, _ ->
+                val user = snapshot?.toObject(User::class.java)?.copy(id = userId)
+                onUserChanged(user)
+            }
+    }
+}
