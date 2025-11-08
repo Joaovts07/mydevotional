de4aa9e72ce8c01000f07699a38bbe575dd7d300
João Vitor Targino <joakim.vts@gmail.com>
2025-11-08 17:31:19 -0300

    MYD-40: Refactor optimize imports

--- app/src/main/java/com/example/mydevotional/viewmodel/DailyReadingViewModel.kt
+++ app/src/main/java/com/example/mydevotional/viewmodel/DailyReadingViewModel.kt
@@ -5,7 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.example.mydevotional.extensions.formatDate
 import com.example.mydevotional.usecase.CompleteReadingsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.Date
 import javax.inject.Inject
